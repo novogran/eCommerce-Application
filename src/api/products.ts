87d32249ba707578bd38f40193de5998e2593bae
Vlad S <vlad_semenenko_2016@mail.ru>
2025-05-29 13:31:51 +0300

    feat: product by key, access token fix

--- src/api/products.ts
+++ src/api/products.ts
@@ -3,11 +3,12 @@ import { authService } from "./auth-client";
 import axios from "axios";
 import { handleRequestError, API_URL } from "../shared/utils/axios-config";
 import type { Product } from "../shared/types/product.types";
+import { getAuthToken } from "../shared/utils/auth-token";
 
 export const productService = {
   async getProducts(params?: URLSearchParams): Promise<Product[]> {
     try {
-      const { access_token } = await authService.getAnonymousToken();
+      const access_token = getAuthToken() ?? (await authService.getAnonymousToken());
       const response: AxiosResponse<{ results: Product[] }> = await axios.get(
         `${API_URL}/product-projections`,
         {
@@ -28,10 +29,29 @@ export const productService = {
 
   async getProductById(productId: string): Promise<Product> {
     try {
-      const { access_token } = await authService.getAnonymousToken();
+      const access_token = getAuthToken() ?? (await authService.getAnonymousToken());
 
       const response: AxiosResponse<Product> = await axios.get(
         `${API_URL}/product-projections/${productId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      return handleRequestError(error);
+    }
+  },
+
+  async getProductByKey(productKey: string): Promise<Product> {
+    try {
+      const access_token = getAuthToken() ?? (await authService.getAnonymousToken());
+
+      const response: AxiosResponse<Product> = await axios.get(
+        `${API_URL}/product-projections/key=${productKey}`,
         {
           headers: {
             Authorization: `Bearer ${access_token}`,
