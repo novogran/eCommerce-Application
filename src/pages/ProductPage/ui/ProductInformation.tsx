d4649845a27b331b24be4a64fc3f278b947d59ec
c00b3r <sanuyOKmaklaren0@gmail.com>
2025-06-01 14:30:10 +0500

    feat: enhance product information display with pricing and discount details

--- src/pages/ProductPage/ui/ProductInformation.tsx
+++ src/pages/ProductPage/ui/ProductInformation.tsx
@@ -13,13 +13,55 @@ function ProductInformation({ data }: { data: Product }) {
         }}
       >
         {data.masterVariant.images && <ProductSlider images={data.masterVariant.images} />}
-        <Box p={2}>
+        <Box p={2} display={"flex"} flexDirection={"column"} gap={2}>
           <Typography variant="h4" fontWeight={600}>
             {data.name["en-US"]}
           </Typography>
           <Typography variant="body1">
             {data.description ? data.description["en-US"] : "Empty description"}
           </Typography>
+          <Box display={"flex"}>
+            {data.masterVariant?.prices && data.masterVariant?.prices[0].discounted ? (
+              <Box display={"flex"}>
+                <Typography
+                  variant="h5"
+                  sx={{ fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem" } }}
+                  component="h1"
+                  px={1}
+                  py={0.5}
+                  color={"red"}
+                >
+                  {data.masterVariant?.prices[0].discounted.value.centAmount / 100}
+                </Typography>
+                <Typography
+                  component="p"
+                  px={0.5}
+                  sx={{
+                    textDecoration: "line-through",
+                    fontSize: { xs: "0.75rem", sm: "1rem", md: "1.25rem" },
+                  }}
+                >
+                  {data.masterVariant?.prices[0].value.centAmount / 100}
+                </Typography>
+              </Box>
+            ) : (
+              <Typography
+                variant="h5"
+                sx={{ fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem" } }}
+                component="h1"
+                px={1}
+                py={0.5}
+              >
+                {data.masterVariant?.prices && data.masterVariant?.prices[0].value.centAmount / 100}
+              </Typography>
+            )}
+            <Typography
+              variant="h6"
+              sx={{ fontSize: { xs: "1.25rem", sm: "1.5rem", md: "1.75rem" } }}
+            >
+              {data.masterVariant?.prices && data.masterVariant?.prices[0].value.currencyCode}
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
