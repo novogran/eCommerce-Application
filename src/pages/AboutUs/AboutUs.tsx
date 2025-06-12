2c6f27e1b1b97476cf90d6b077a5567858a097f4
Vlad S <vlad_semenenko_2016@mail.ru>
2025-06-12 18:51:35 +0300

    feat: information about team

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,17 +1,23 @@
 import { Box, Container, Typography } from "@mui/material";
 import { Link } from "react-router";
+import CreatorCard from "./CreatorCard";
 
 function AboutUs() {
   return (
     <Container>
       <Typography variant="h3" textAlign={"center"} my={2}>
-        About Us
+        DreamTeam - About Us
+      </Typography>
+      <Typography variant="h6" textAlign={"center"} my={2}>
+        Our friendly team acted cohesively, helping each other when problems arose. At meetings,
+        which took place every 2 days, we discussed what we managed to realize and what caused
+        problems, divided tasks, and acted according to deadlines.
       </Typography>
       <Box display={"flex"} flexWrap={"wrap"} justifyContent={"center"} gap={2}>
         <CreatorCard
-          name="Vladislav Semenenko"
+          name="Semenenko Vladislav"
           job="Student"
-          bio="Currently studying to become a FrontEnd Developer. 4 course"
+          bio="From Minsk, currently studying at BSUIR in the 4th year on Game Design."
           doneFeatures={[
             "Catalog Page",
             "Login Page Layout",
@@ -20,35 +26,35 @@ function AboutUs() {
             "404 Page",
           ]}
           githubLink="https://github.com/z1ll1ax"
+          photoUrl="/VladislavAvatar.jpg"
+        ></CreatorCard>
+        <CreatorCard
+          name="Novogran Vitaly"
+          job="Student. Teamlead"
+          bio="Currently studying to become a FrontEnd Developer. 4 course"
+          doneFeatures={[
+            "Validation",
+            "Profile Page",
+            "Basket Page",
+            "Authentication",
+            "Repository and Task Board Setup",
+          ]}
+          githubLink="https://github.com/novogran"
           photoUrl="/404.jpg"
         ></CreatorCard>
         <CreatorCard
-          name="Vladislav Semenenko"
-          job="Student"
-          bio="Currently studying to become a FrontEnd Developer. 4 course"
+          name="Vasilev Aleksandr"
+          job="Frontend developer"
+          bio="From Yekaterinburg, graduated from UrFU in Software Engineering. Currently studying for a master's degree in Human-Computer Interaction."
           doneFeatures={[
-            "Catalog Page",
-            "Login Page Layout",
-            "Registration Page Layout",
-            "About Us Page",
-            "404 Page",
+            "Detailed Product Page",
+            "Header",
+            "Routing",
+            "Authentication",
+            "State Management",
           ]}
-          githubLink="https://github.com/z1ll1ax"
-          photoUrl="/404.jpg"
-        ></CreatorCard>
-        <CreatorCard
-          name="Vladislav Semenenko"
-          job="Student"
-          bio="Currently studying to become a FrontEnd Developer. 4 course"
-          doneFeatures={[
-            "Catalog Page",
-            "Login Page Layout",
-            "Registration Page Layout",
-            "About Us Page",
-            "404 Page",
-          ]}
-          githubLink="https://github.com/z1ll1ax"
-          photoUrl="/404.jpg"
+          githubLink="https://github.com/c00b3r"
+          photoUrl="/AleksandrAvatar.jpg"
         ></CreatorCard>
       </Box>
       <Link to="https://rs.school/" target="_blank">
@@ -65,68 +71,6 @@ function AboutUs() {
         </Box>
       </Link>
     </Container>
-  );
-}
-
-type CreatorCardProps = {
-  name: string;
-  job: string;
-  bio: string;
-  doneFeatures: string[];
-  githubLink: string;
-  photoUrl: string;
-};
-
-function CreatorCard({ name, job, bio, doneFeatures, githubLink, photoUrl }: CreatorCardProps) {
-  return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      borderRadius={2}
-      boxShadow={3}
-      maxWidth={"32%"}
-      minWidth={"20rem"}
-      justifyContent={"baseline"}
-      alignItems={"center"}
-      position={"relative"}
-      p={1}
-      bgcolor={"White"}
-    >
-      <Box
-        component="img"
-        src={photoUrl}
-        alt={name}
-        mb={2}
-        sx={{
-          width: { xs: "90%", sm: "100%" },
-          maxWidth: "250px",
-          borderRadius: "50%",
-          border: "2px black solid",
-        }}
-      />
-      <Typography variant="h5">{name}</Typography>
-      <Typography variant="h6" mb={2}>
-        {job}
-      </Typography>
-      <Typography textAlign={"center"} mb={2}>
-        {bio}
-      </Typography>
-      {doneFeatures.map((feat, index) => {
-        return <Typography key={index}>{feat}</Typography>;
-      })}
-      <Box position={"absolute"} right={"0.5rem"}>
-        <Link to={githubLink} target="_blank">
-          <Box
-            component="img"
-            src="/github.svg"
-            alt="name"
-            sx={{
-              maxWidth: "65px",
-            }}
-          />
-        </Link>
-      </Box>
-    </Box>
   );
 }
 
---
+++ src/pages/AboutUs/CreatorCard.tsx
@@ -0,0 +1,90 @@
+import { Box, Divider, Typography } from "@mui/material";
+import { Link } from "react-router";
+
+type CreatorCardProps = {
+  name: string;
+  job: string;
+  bio: string;
+  doneFeatures: string[];
+  githubLink: string;
+  photoUrl: string;
+};
+
+function CreatorCard({ name, job, bio, doneFeatures, githubLink, photoUrl }: CreatorCardProps) {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      borderRadius={2}
+      boxShadow={3}
+      maxWidth={"32%"}
+      minWidth={"20rem"}
+      justifyContent={"baseline"}
+      alignItems={"center"}
+      position={"relative"}
+      p={1}
+      bgcolor={"White"}
+    >
+      <Box
+        mb={2}
+        sx={{
+          width: { xs: "150px", md: "180px" },
+          height: { xs: "150px", md: "180px" },
+          borderRadius: "50%",
+          boxShadow: 3,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          border: "2px black solid",
+        }}
+      >
+        <Box
+          component="img"
+          src={photoUrl}
+          alt={name}
+          sx={{
+            width: "100%",
+          }}
+        />
+      </Box>
+      <Typography variant="h4" textAlign={"center"}>
+        {name}
+      </Typography>
+      <Typography variant="h5" mb={2}>
+        {job}
+      </Typography>
+      <Divider orientation="horizontal" sx={{ width: "100%" }}></Divider>
+      <Typography textAlign={"center"} m={1} height={"7rem"}>
+        {bio}
+      </Typography>
+      <Divider orientation="horizontal" sx={{ width: "100%" }}></Divider>
+      <Typography textAlign={"center"} mb={2} variant="h6">
+        Worked on:
+      </Typography>
+      <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}>
+        {doneFeatures.map((feat, index) => {
+          return (
+            <Typography key={index} textAlign={"center"}>
+              {feat}
+            </Typography>
+          );
+        })}
+      </Box>
+      <Box position={"absolute"} right={"0.5rem"}>
+        <Link to={githubLink} target="_blank">
+          <Box
+            component="img"
+            src="/github.svg"
+            alt="name"
+            sx={{
+              maxWidth: "65px",
+            }}
+          />
+        </Link>
+      </Box>
+    </Box>
+  );
+}
+
+export default CreatorCard;
